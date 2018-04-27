a45cb0a49efbe1e56749d7eea7a9f407bded64c4
FrannyRC96 <fransiskino1996@gmail.com>
2018-04-27 09:45:24 -0700

    Add files via upload

---
+++ Xelmet/src/mx/itson/xelmet/nucleo/entidades/Cliente.java
@@ -0,0 +1,18 @@
+
+package mx.itson.xelmet.nucleo.entidades;
+
+import java.util.List;
+
+/**
+ *
+ * @author fcoav
+ */
+public class Cliente {
+   int idCliente;
+   String nombre, numeroTelefonico, domicilio;
+   Paquete idPaquete;
+   List <Llamadas> listaLlamada;
+   
+   
+    
+}
