88e1a79262c11b2340eac8b2b0947e216cc1818e
javiershaka <32804342+javiershaka@users.noreply.github.com>
2018-05-08 16:59:56 -0700

    Actualizacion de base de datos
    Base de dato final

--- xemletbd.sql
+++ xemletbd.sql
@@ -33,7 +33,7 @@ CREATE TABLE `cliente` (
   PRIMARY KEY (`id`),
   KEY `FK_cliente_to_paquete_idx` (`paquete`),
   CONSTRAINT `FK_cliente_paquete` FOREIGN KEY (`paquete`) REFERENCES `paquete` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,8 +42,42 @@ CREATE TABLE `cliente` (
 
 LOCK TABLES `cliente` WRITE;
 /*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
-INSERT INTO `cliente` VALUES (1,'Paquete sin limites','6222277534','Periferico 81',1);
+INSERT INTO `cliente` VALUES (1,'Juanca','6222277534','Periferico 81',1),(15,'9','9','9',1);
 /*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `factura`
+--
+
+DROP TABLE IF EXISTS `factura`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `factura` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idCliente` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `fecha` varchar(45) DEFAULT NULL,
+  `llamadasMovil` int(11) DEFAULT NULL,
+  `llamadasFijas` int(11) DEFAULT NULL,
+  `llamadasRealizadas` int(11) NOT NULL,
+  `costoPaquete` decimal(6,2) NOT NULL,
+  `cargosAdicionales` decimal(6,2) DEFAULT NULL,
+  `total` decimal(6,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_factura_cliente_idx` (`idCliente`),
+  CONSTRAINT `FK_factura_cliente` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `factura`
+--
+
+LOCK TABLES `factura` WRITE;
+/*!40000 ALTER TABLE `factura` DISABLE KEYS */;
+INSERT INTO `factura` VALUES (1,1,'1','1',1,1,1,1.00,1.00,1.00),(5,15,'9','Febrero/2016',62,1,4,1.00,61.00,62.00),(6,15,'9','Febrero/2016',62,1,4,1.00,61.00,62.00),(7,15,'9','Febrero/2016',62,1,4,1.00,61.00,62.00);
+/*!40000 ALTER TABLE `factura` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -59,11 +93,11 @@ CREATE TABLE `llamada` (
   `tipoLlamada` varchar(45) DEFAULT NULL,
   `duracionMinutos` varchar(45) DEFAULT NULL,
   `fecha` varchar(45) DEFAULT NULL,
-  `idReferencial` int(11) DEFAULT NULL,
+  `idReferencial` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_llamadas_idx` (`idReferencial`),
   CONSTRAINT `FK_llamadas` FOREIGN KEY (`idReferencial`) REFERENCES `cliente` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,7 +106,7 @@ CREATE TABLE `llamada` (
 
 LOCK TABLES `llamada` WRITE;
 /*!40000 ALTER TABLE `llamada` DISABLE KEYS */;
-INSERT INTO `llamada` VALUES (16,'6222222222','FIJA','15','25/05/2016',1),(17,'6222222222','FIJA','15','25/05/2016',1),(18,'6222288794','MOVIL','4','25/01/2016',NULL),(19,'6452288797','FIJA','6','1/01/2010',NULL),(20,'687894894','MOVIL','4','1/01/2011',NULL),(21,'6444455779','MOVIL','4','25/01/0001',1);
+INSERT INTO `llamada` VALUES (29,'6222277534','MOVIL','14','4/Febrero/2016',15),(30,'62264564','FIJA','4','3/Febrero/2016',15),(31,'6222277534','MOVIL','16','3/Febrero/2016',15),(32,'622222222','MOVIL','32','3/Febrero/2016',15);
 /*!40000 ALTER TABLE `llamada` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -92,7 +126,7 @@ CREATE TABLE `paquete` (
   `costoAdicionalPorLlamada` int(11) DEFAULT NULL,
   `costoAdicionalPorMinitos` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -101,7 +135,7 @@ CREATE TABLE `paquete` (
 
 LOCK TABLES `paquete` WRITE;
 /*!40000 ALTER TABLE `paquete` DISABLE KEYS */;
-INSERT INTO `paquete` VALUES (1,'Paquete Sin limites',1,1,1.00,1,1),(5,'Paquete codo',8,8,8.00,8,8),(6,'PaqueteAlcance',4,123,123.00,21312,12);
+INSERT INTO `paquete` VALUES (1,'Paquete Sin limites',1,1,1.00,1,1),(5,'8',8,8,8.00,8,8),(6,'PaqueteAlcance',4,123,123.00,21312,12),(7,'9',9,9,9.00,9,9);
 /*!40000 ALTER TABLE `paquete` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -122,4 +156,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-06 20:07:01
+-- Dump completed on 2018-05-08 16:52:59
